85607b732b2c2f931330ba2e81b51523dbd814cf
Your Name <you@example.com>
2020-04-28 23:58:30 +0300

    sh file updated

--- parse.sh
+++ parse.sh
@@ -6,42 +6,42 @@ source env/bin/activate
 
 cd parsing
 
-python inst_by_tag.py 'бессмертныйполкспб'
+python inst_by_tag.py 0
 wait
 echo "..Parsing finished. Wait for next..."
 sleep 60s
 
-python inst_by_tag.py 'бессмертныйполкмосква'
+python inst_by_tag.py 1
 wait
 echo "..Parsing finished. Wait for next..."
 sleep 60s
 
-python inst_by_tag.py 'бессмертныйполккраснодар'
+python inst_by_tag.py 2
 wait
 echo "..Parsing finished. Wait for next..."
 sleep 60s
 
-python inst_by_tag.py 'бессмертныйполкказань'
+python inst_by_tag.py 3
 wait
 echo "..Parsing finished. Wait for next..."
 sleep 60s
 
-python inst_by_tag.py 'бессмертныйполкуфа'
+python inst_by_tag.py 4
 wait
 echo "..Parsing finished. Wait for next..."
 sleep 60s
 
-python inst_by_tag.py 'бессмертныйполктюмень'
+python inst_by_tag.py 5
 wait
 echo "..Parsing finished. Wait for next..."
 sleep 60s
 
-python inst_by_tag.py 'бессмертныйполксочи'
+python inst_by_tag.py 6
 wait
 echo "..Parsing finished. Wait for next..."
 sleep 60s
 
-python inst_by_tag.py 'бессмертныйполкомск'
+python inst_by_tag.py 7
 wait
 echo "..Parsing finished. All done"
 
