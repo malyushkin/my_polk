e7c2e5f9fd9b5c44b01f0273179b632c9049a50d
Roman <malyushkinr@gmail.com>
2020-04-29 01:29:13 +0300

    sh extended

--- parse.sh
+++ parse.sh
@@ -47,5 +47,80 @@ sleep 60s
 python inst_by_tag.py 7
 wait
 echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 8
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 9
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 10
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 11
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 12
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 13
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 14
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 15
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 16
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 17
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 18
+wait
+echo "..Parsing finished. All done"sleep 60s
+sleep 60s
+
+python inst_by_tag.py 19
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 20
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 21
+wait
+echo "..Parsing finished. All done"
+sleep 60s
+
+python inst_by_tag.py 22
+wait
+echo "..Parsing finished. All done"
 
 deactivate
