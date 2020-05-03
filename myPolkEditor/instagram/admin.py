985f08c02ced75147bb158329cdfba3c45ca1b89
Roman <malyushkinr@gmail.com>
2020-05-03 13:39:52 +0300

    add pic to admin

--- myPolkEditor/instagram/admin.py
+++ myPolkEditor/instagram/admin.py
@@ -4,12 +4,17 @@ from .models import *
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('shortcode', 'get_post_url', 'published', 'likes_count', 'comments_count', 'caption', 'query',
-                    'is_video', 'video_view_count')
+    list_display = ('shortcode', 'get_display_url', 'get_post_url', 'published', 'likes_count', 'comments_count',
+                    'caption', 'query', 'is_video', 'video_view_count')
+    list_filter = ('is_video', 'query')
     readonly_fields = ['id', 'owner_id', 'shortcode', 'is_video', 'query']
     search_fields = ('caption', 'query')
 
-    list_filter = ('is_video', 'query')
+    def get_display_url(self, obj):
+        if obj.display_url:
+            return mark_safe(f'<img src="{obj.display_url}" width="100px" />')
+        else:
+            return '-'
 
     def get_post_url(self, obj):
         if obj.display_url:
